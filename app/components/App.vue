bcb8b17c9e05a50a47ff3abd1cea6dc889d79fe7
Jan Plazovnik <jan.plazovnik@gmail.com>
2019-10-18 01:31:05 +0200

    Add on to the code; edit design, rework some mechanics

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,90 +1,198 @@
 <template>
-  <Page>
-    <ActionBar title="Weather" />
+  <Page @loaded="pageLoaded">
+    <ActionBar>
+      <StackLayout orientation="horizontal">
+        <Label text="Weather" fontSize="20" verticalAlignment="center" color="#ffffff"/>
+      </StackLayout>
+    </ActionBar>
+    <PullToRefresh @refresh="refreshList" class="refresh">
+      <StackLayout class="p-20">
+        <Label textWrap="true" class="text-center temperature-view temperatureCityColor">
+          <FormattedString v-if="weather.main">
+            <Span :text="(weather.main.temp % 1 < 0.5) ? Math.floor(weather.main.temp) : Math.ceil(weather.main.temp)" />
+            <Span text=" °C" />
+          </FormattedString>
+        </Label>
+        <Label :text="weather.name" class="h3 text-center temperatureCityColor" />
+        <Label :text="lastUpdated" class="h4 text-center lastUpdatedColor" />
 
-    <StackLayout class="p-20">
-      <!-- <Label v-if="weather.main" :text="weather.main.temp" class="text-center temperature-view" /> -->
-      <Label textWrap="true" class="text-center temperature-view">
-        <FormattedString>
-          <Span v-if="weather.main" :text="weather.main.temp"/>
-          <Span text=" °C"/>
-        </FormattedString>
-      </Label>
-      <Label :text="weather.name" class="h2 text-center" />
-      
-      <FlexboxLayout alignItems="center" justifyContent="space-around" style="margin-top: 30px; margin-bottom: 30px;">
-        <Button width="33%" text="Today" @tap="onButtonTap(1)" />
-        <Button width="33%" text="Tomorrow" @tap="onButtonTap(2)" />
-        <Button width="33%" text="Other" @tap="onButtonTap(3)" />
-      </FlexboxLayout>
 
-      <FlexboxLayout v-if="currentViewIndex==1" alignItems="center" justifyContent="space-around">
-        <Label text="val1" width="33%" height="200" backgroundColor="#488c53" class="text-center hourly-view" />
-        <Label text="val2" width="33%" height="200" backgroundColor="#488c53" class="text-center hourly-view" />
-        <Label text="val3" width="33%" height="200" backgroundColor="#488c53" class="text-center hourly-view" />
-      </FlexboxLayout>      
-      <FlexboxLayout v-if="currentViewIndex==2" alignItems="center" justifyContent="space-around">
-        <Label text="val4" width="33%" height="200" backgroundColor="#488c53" class="text-center hourly-view" />
-        <Label text="val5" width="33%" height="200" backgroundColor="#488c53" class="text-center hourly-view" />
-        <Label text="val6" width="33%" height="200" backgroundColor="#488c53" class="text-center hourly-view" />
-      </FlexboxLayout>      
-      <FlexboxLayout v-if="currentViewIndex==3" alignItems="center" justifyContent="space-around">
-        <Label text="val7" width="33%" height="200" backgroundColor="#488c53" class="text-center hourly-view" />
-        <Label text="val8" width="33%" height="200" backgroundColor="#488c53" class="text-center hourly-view" />
-        <Label text="val9" width="33%" height="200" backgroundColor="#488c53" class="text-center hourly-view" />
-      </FlexboxLayout>      
+        <SegmentedBar>
+          <SegmentedBarItem title="Today" />
+          <SegmentedBarItem title="Tomorrow" />
+          <SegmentedBarItem title="Other" />
+        </SegmentedBar>
 
-    </StackLayout>
+        <Label text="Extra information" class="h2 temperatureCityColor" />
+        <GridLayout columns="2*, 2*" rows="2*, 2*" height="200px">
+          <Label textWrap="true" row="0" col="0" horizontalAlignment="center" verticalAlignment="center" class="temperatureCityColor">
+            <FormattedString v-if="weather.wind">
+              <Span text="Wind speed: " fontWeight="bold"/>
+              <Span :text="weather.wind.speed" />
+              <Span text=" m/s" />
+            </FormattedString>
+          </Label>          
+          <Label textWrap="true" row="0" col="1" horizontalAlignment="center" verticalAlignment="center" class="temperatureCityColor">
+            <FormattedString v-if="weather.main">
+              <Span text="Humidity: " fontWeight="bold"/>
+              <Span :text="weather.main.humidity" />
+              <Span text="%" />
+            </FormattedString>
+          </Label>          
+          <Label textWrap="true" row="1" col="0" horizontalAlignment="center" verticalAlignment="center" class="temperatureCityColor">
+            <FormattedString v-if="weather.main">
+              <Span text="Air pressure: " fontWeight="bold"/>
+              <Span :text="weather.main.pressure" />
+              <Span text=" mb" />
+            </FormattedString>
+          </Label>          
+          <Label textWrap="true" row="1" col="1" horizontalAlignment="center" verticalAlignment="center" class="temperatureCityColor">
+            <FormattedString v-if="weather.visibility">
+              <Span text="Visibility: " fontWeight="bold"/>
+              <Span :text="weather.visibility / 1000" />
+              <Span text=" km" />
+            </FormattedString>
+          </Label>              
+        </GridLayout>
+      </StackLayout>
+    </PullToRefresh>
+    
   </Page>
 </template>
 
 <script >
+import * as app from "tns-core-modules/application";
+import * as platform from "tns-core-modules/platform";
+import * as color from "tns-core-modules/color";
 import * as axios from "axios";
+import * as moment from "moment";
+import * as geolocation from "nativescript-geolocation";
+import { Accuracy } from "tns-core-modules/ui/enums";
 export default {
   data() {
     return {
-      temperature: "x °C",
-      area: "Foo, Bar",
       currentViewIndex: 1,
-      weather: {}
+      lastUpdated: null,
+      weather: {
+        weather: {
+          0: {
+            main: "",
+            description: ""
+          }
+        },
+        main: {
+          temp: 0,
+          pressure: 0,
+          humidity: 0,
+          temp_min: 0,
+          temp_max: 0
+        },
+        visibility: 0,
+        wind: {
+          speed: 0,
+          deg: 0
+        },
+        name: ""
+      }
     };
   },
   methods: {
-    onButtonTap(args) {
-      this.currentViewIndex = args;
-      // action(`${this.weather.main.temp}`, "OK")
-      //   .then(result => {
-      //     console.log(result);
-      //   });
-      this.area = this.weather.main.temp;
-      console.log(this.weather.main.temp);
-      console.log("view index changed to: " + args);
+    refreshList(args) {
+      var pullRefresh = args.object;
+      pullRefresh.refreshing = false;
+      this.getLocation();
+    },
+    getLocation() {
+      geolocation
+        .getCurrentLocation({
+          desiredAccuracy: Accuracy.high,
+          maximumAge: 5000,
+          timeout: 10000
+        })
+        .then(
+          loc => {
+            if (loc) {
+              this.fetchWeather(loc);
+            }
+          },
+          e => {
+            console.log("Error: " + (e.message || e));
+            this.getLocation();
+          }
+        );
+    },
+    fetchWeather(loc) {
+      axios
+        .get(
+          // `https://api.openweathermap.org/data/2.5/forecast?lat=${loc.latitude}&lon=${loc.longitude}&units=metric&appid=806b02f9dab6137941fb03eab18d9984`
+          `https://api.openweathermap.org/data/2.5/weather?lat=${loc.latitude}&lon=${loc.longitude}&units=metric&appid=806b02f9dab6137941fb03eab18d9984`
+        )
+        .then(res => {
+          this.weather = res.data;
+          this.lastUpdated = `Last updated at ${moment(loc.timestamp).format('h:mm:ss a')}`;
+        })
+        .catch(res => console.log(res));    
+    },
+    pageLoaded() {
+      if (app.android && platform.device.sdkVersion >= "21") {
+        const window = app.android.foregroundActivity.getWindow();
+        window.setStatusBarColor(new color.Color("#5876fc").android);
+      }
     }
   },
   mounted() {
-    axios
-      .get('https://api.openweathermap.org/data/2.5/weather?lat=46.36174779999999&lon=15.113785&units=metric&appid=1025f8b1bb1e5b77c94a6f233ed1849f')
-      .then(res => {console.log("success"); this.weather = res.data});
+    this.getLocation();
   }
 };
 </script>
 
 <style scoped>
 ActionBar {
-  background-color: #53ba82;
+  background-color: #5876fc;
   color: #ffffff;
 }
+Page {
+  background-color: #ffffff;
+}
 
-label {
+Label {
   color: #000;
 }
 
-.temperature-view {
-  font-size: 55px;
+.temperatureCityColor {
+  color: #4e598c;
 }
 
-.hourly-view {
-  border-radius: 30px;
+.lastUpdatedColor {
+  color: #7783b8;
+}
+
+.temperature-view {
+  font-size: 75px;
+}
+
+.hourlyView {
   margin: 10px;
+}
+
+.stackViewFirstChild {
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+}
+
+.stackViewLastChild {
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+}
+
+.viewSwitchButton {
+  background-color: #8499f9;
+  /* border-bottom: 1px solid #8499f9; */
+  border-radius: 10px;
+  margin: 10px;
+  outline: none;
+}
+.refresh {
+  color: #4e598c;
 }
 </style>
