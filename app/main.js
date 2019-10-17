b6c0c8c2d10a5cabd768c5a0aad6750a569c17a6
Jan Plazovnik <jan.plazovnik@gmail.com>
2019-10-18 01:30:44 +0200

    Register PullToRefresh component

--- app/main.js
+++ app/main.js
@@ -3,8 +3,11 @@ import App from './components/App'
 
 
 
+Vue.registerElement(
+  'PullToRefresh',
+  () => require('@nstudio/nativescript-pulltorefresh').PullToRefresh
+);
 
-  
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
