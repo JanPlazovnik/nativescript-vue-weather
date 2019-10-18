06065cef4c9310cad6b32044bb8540114c68485f
Jan Plazovnik <jan.plazovnik@gmail.com>
2019-10-18 15:29:03 +0200

    Register FAB component

--- app/main.js
+++ app/main.js
@@ -8,6 +8,11 @@ Vue.registerElement(
   () => require('@nstudio/nativescript-pulltorefresh').PullToRefresh
 );
 
+Vue.registerElement(
+  'Fab',
+  () => require('@nstudio/nativescript-floatingactionbutton').Fab
+);
+
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
